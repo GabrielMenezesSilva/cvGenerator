5999922ba958af7eb368cb18f563c983c1983ca7
GabrielMenezesSilva <mrgabrielmenezes@icloud.com>
2024-07-25 12:05:10 +0200

    Comentarios v2

--- src/App.jsx
+++ src/App.jsx
@@ -11,9 +11,13 @@ import { ProtectedRoute, PublicRoute } from "./components/routes";
 function App() {
   const navigate = useNavigate();
 
+  // Função para armazenar o token do usuário no localStorage e redirecionar para a página /cv
   function storageUserToken(user) {
+    // Verifica se o objeto user e o accessToken existem
     if (user && user.accessToken) {
+      // Armazena o accessToken no localStorage com a chave 'userToken'
       localStorage.setItem("userToken", user.accessToken);
+      // Redireciona o usuário para a rota /cv
       navigate("/cv");
     }
   }
--- src/components/routes.jsx
+++ src/components/routes.jsx
@@ -1,13 +1,17 @@
 import React from 'react';
 import { Navigate } from 'react-router-dom';
 
+// Define o componente ProtectedRoute, que recebe children como props
 function ProtectedRoute({children}){
+    // Obtém o token do usuário do localStorage
     const userToken = localStorage.getItem('userToken');
+    // Se o token existir, renderiza os children, caso contrário, redireciona para a página de login
     return userToken ? children : <Navigate to="/login"/>;
 }
 
 function PublicRoute({children}){
     const userToken = localStorage.getItem('userToken');
+    // Se o token existir, redireciona para a página /cv, caso contrário, renderiza os children
     return userToken ? <Navigate to="/cv"/> : children;
 }
 
